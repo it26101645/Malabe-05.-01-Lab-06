957444216d87f19d4a8ef5f6478b38f061e181c1
it26101645 <it26101645@my.sliit.lk>
2026-09-22 09:09:34 +0530

    Add files via upload

---
+++ Q2/IT26101645Lab6Q2A.java
@@ -0,0 +1,11 @@
+public class IT26101645Lab6Q2A {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i <= 10) {
+            System.out.print(i + " ");
+            i++;
+        }
+    }
+}
